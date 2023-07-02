773e396f0b872e6b6dfdc28cc1273cc2876a80d5
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-07-02 14:59:07 -0600

    Test2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import gateway from 'express-gateway';
 import { fileURLToPath } from 'url';
-import './test';
+import './test.js';
 
 const CURRENT_DIR = path.dirname(fileURLToPath(import.meta.url));
 const union = path.join(CURRENT_DIR, '/config');
